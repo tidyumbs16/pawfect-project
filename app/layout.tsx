879677698cc9f8c29f244700f5fc5bc2e96ac563
tidyumbs16 <deemeesri456@gmail.com>
2025-12-05 20:46:38 +0700

    layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-  import Navbar from "@/components/navbar";
+import Navbar from "@/components/navbar";
+
+export const dynamic = "force-dynamic";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
