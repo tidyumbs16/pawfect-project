2ea807340c7c32f910c0611a0ffeb84ae82936a2
tidyumbs16 <deemeesri456@gmail.com>
2025-12-05 17:53:40 +0700

    edit prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
+// ใช้ Prisma + Accelerate (แนะนำ Next.js 14/16)
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query", "error", "warn"],
-  });
+  globalForPrisma.prisma ??
+  new PrismaClient().$extends(withAccelerate());
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma as unknown as PrismaClient | undefined;
+}
